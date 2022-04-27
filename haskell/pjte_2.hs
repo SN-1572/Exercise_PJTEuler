e40024684425537d3310e4bbeb4c698fef8c3cb2
SN-1572 <104142293+SN-1572@users.noreply.github.com>
2022-04-27 19:19:44 +0900

    Update pjte_2.hs

--- haskell/pjte_2.hs
+++ haskell/pjte_2.hs
@@ -9,19 +9,10 @@ main = do
  putStrLn $ show(result)
 
 
-threefivelist_below :: Int -> [Int]
-
-threefivelist_below  a
- | a == 0 = []
- | a `mod` 3 == 0 = a : threefivelist_below (a-1)
- | a `mod` 5 == 0 = a : threefivelist_below (a-1)
- | otherwise = threefivelist_below (a-1)
-
 iseven :: Int -> Bool
 iseven a
  | a `mod` 2 == 0 = True
  | otherwise = False
-
 
 fibonacciseq_entry :: Int -> [Int]
 fibonacciseq_entry a = (1 : 1 : (fibonacciseq a 1 1))
